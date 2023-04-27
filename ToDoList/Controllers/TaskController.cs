651694a7f0b5b143e5d27b355beef61978e02645
artur <zeroone02@mail.ru>
2023-04-27 21:53:37 +0300

    Удалил ненужные файлы, переименовал HomeController в TaskController

---
+++ ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ToDoList.Controllers;
+public class TaskController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
